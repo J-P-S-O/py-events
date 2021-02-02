5dd714d5256a9ceb13b0cd463b0be14339dde0a9
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-02 10:15:00 -0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 class EventEmitter{
-
+    def _init_(self):
+        self.map = 
+    def on(self, event, handler):
+        map = self.map
+    def emit(self, event, args):
+        
+    def remove(self, event, handler):
+        
+    
 }
