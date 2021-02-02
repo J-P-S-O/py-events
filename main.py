8c83b49bc41449064bacf270412ef23bd10ca271
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-02 11:32:02 -0300

    .

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 class EventEmitter{
     def _init_(self):
-        self.map = 
+        self.map = {
+            
+        }
     def on(self, event, handler):
-        map = self.map
+        if not callable(handler)
+            throw "TypeError: Handler must be function"
+            
+            
+        if (self.map.get(event, None) != None)
+            self.map[event].append(handler)
+        else
+            self.map[event] = []
+            self.map[event].append(handler)
     def emit(self, event, args):
+        if not self.map[event]
+            return
+        
+        for i in self.map[event]:
+            i(event, args)
         
     def remove(self, event, handler):
+        if not handler:
+            self.map[event] = []
+        else
+            j = 0
+            for i in self.map[event]:
+                
+                if i == handler:
+                    self.map[event].pop(j)
+                else:
+                    pass
+                j = j+1
         
     
 }
---
+++ test.py
@@ -0,0 +1,4 @@
+
+# Test file
+from main import *
+App = EventEmitter()
